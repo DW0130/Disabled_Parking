26441554da95233f5f4abc6730c85aa3cfc07933
정다운 <dw010130@kakao.com>
2022-02-09 00:14:45 +0900

    번호판 판별 new버전

--- CarNumber/run.py
+++ CarNumber/run.py
@@ -1,7 +1,8 @@
 
 import re
+from unittest import result
 
-num = "149우9815호1나"
+num = "자149우9815호1나"
 
 print(num) # 입력된 번호판 값
 
@@ -10,41 +11,35 @@ string = num
 C_Num = re.findall('\w', string) # 문자열 -> 문자만 # re.findall('\w', string)  = 1개씩 분리
 
 # print(numbers)
-# print(C_Num) # 번호판 쪼개기
+print(C_Num) # 번호판 쪼개기
 
-for data in C_Num:
-    if(data.isalpha()): # 문자가 있는지 판별
-        Check_Data = C_Num.index(data)
-        if(C_Num[Check_Data -1]):
-            
-        
-        # data1 = data
-        
+data1 = num[0:7]
+data2 = num[0:8]
+print(data1)
+print(data2)
+
+for data in data1:
+    if(data.isalpha()):
+        Check_Data = data1.index(data)
         break
 print(Check_Data)
+print("======================")
+if(Check_Data == 0):
+    data1 = num[1:8]
+    print(data1)
+# for문사용하여 1번째 for문은 번호판이 7개 기준, 2번째 for문은 8개 기준으로 계산
+
+
+# for data in C_Num:
+#     if(data.isalpha()): # 문자가 있는지 판별
+#         Check_Data = C_Num.index(data)
+            
+        
+#         # data1 = data
+        
+#         break
+# print(Check_Data)
 # in_data = C_Num.index(data1) # 좌측에서 첫번째 글자 위치
-
-n = -1
-while True:
-    if C_Num[n+1:].count(0) == 0:
-        break
-
-
-
-
-
-
-
-list_a = [1, 1, 1, 2, 3, 4, 1, 5, 6, 1, 1]
-
-n = -1
-while True:
-    if list_a[n+1:].count(1) == 0:
-        break
-    n += list_a[n+1:].index(1) + 1
-    print(n, end=' ')
-[출처] [python] list에서 특정값의 인덱스 찾기|작성자 피티아
-
 
 
 # data2 = C_Num[in_data -1]
