a928b1964f54d9219dc30ecc9f64915d722c3a88
DaWoon <dw010130@kakao.com>
2022-02-08 17:16:34 +0900

    이미지 처리된 번호 변환(3자리와 첫번째 자리에 글자가 있을경우 오류)

---
+++ CarNumber/run.py
@@ -0,0 +1,39 @@
+
+import re
+
+num = "149우9815호1나"
+
+print(num) # 입력된 번호판 값
+
+string = num
+#numbers = re.findall('\d+', string) # 문자열 -> 숫자만 
+C_Num = re.findall('\w', string) # 문자열 -> 문자만 # re.findall('\w', string)  = 1개씩 분리
+
+# print(numbers)
+print(C_Num) # 번호판 쪼개기
+
+for data in C_Num:
+    if(data.isalpha()): # 문자가 있는지 판별
+        
+        
+        data1 = data
+        
+        break
+
+in_data = C_Num.index(data1)
+print(data1) # 좌측에서 첫번째 글자
+print(in_data) # 해당글자 위치
+
+
+
+
+data2 = C_Num[in_data -1]
+data3 = C_Num[in_data -2]
+data4 = C_Num[in_data +1]
+data5 = C_Num[in_data +2]
+data6 = C_Num[in_data +3]
+data7 = C_Num[in_data +4]
+
+Car_Number = data3 + data2 + data1 + data4 + data5 + data6 + data7
+
+print(Car_Number)
