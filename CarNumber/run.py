a9e6ba04b73909f927bf5531921d4f4f5ba7a15c
DaWoon <dw010130@kakao.com>
2022-02-08 17:38:17 +0900

    번호판 조회(글자수 확인할것)

--- CarNumber/run.py
+++ CarNumber/run.py
@@ -10,30 +10,50 @@ string = num
 C_Num = re.findall('\w', string) # 문자열 -> 문자만 # re.findall('\w', string)  = 1개씩 분리
 
 # print(numbers)
-print(C_Num) # 번호판 쪼개기
+# print(C_Num) # 번호판 쪼개기
 
 for data in C_Num:
     if(data.isalpha()): # 문자가 있는지 판별
+        Check_Data = C_Num.index(data)
+        if(C_Num[Check_Data -1]):
+            
         
-        
-        data1 = data
+        # data1 = data
         
         break
+print(Check_Data)
+# in_data = C_Num.index(data1) # 좌측에서 첫번째 글자 위치
 
-in_data = C_Num.index(data1)
-print(data1) # 좌측에서 첫번째 글자
-print(in_data) # 해당글자 위치
+n = -1
+while True:
+    if C_Num[n+1:].count(0) == 0:
+        break
 
 
 
 
-data2 = C_Num[in_data -1]
-data3 = C_Num[in_data -2]
-data4 = C_Num[in_data +1]
-data5 = C_Num[in_data +2]
-data6 = C_Num[in_data +3]
-data7 = C_Num[in_data +4]
 
-Car_Number = data3 + data2 + data1 + data4 + data5 + data6 + data7
 
-print(Car_Number)
+
+list_a = [1, 1, 1, 2, 3, 4, 1, 5, 6, 1, 1]
+
+n = -1
+while True:
+    if list_a[n+1:].count(1) == 0:
+        break
+    n += list_a[n+1:].index(1) + 1
+    print(n, end=' ')
+[출처] [python] list에서 특정값의 인덱스 찾기|작성자 피티아
+
+
+
+# data2 = C_Num[in_data -1]
+# data3 = C_Num[in_data -2]
+# data4 = C_Num[in_data +1]
+# data5 = C_Num[in_data +2]
+# data6 = C_Num[in_data +3]
+# data7 = C_Num[in_data +4]
+
+# Car_Number = data3 + data2 + data1 + data4 + data5 + data6 + data7
+
+# print(Car_Number)
